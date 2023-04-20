4740eddc67060e91de2527d629d34fd061b3bc8e
'mjy' <'mso82300@gmail.com'>
2023-04-20 17:32:34 +0900

    dotmap 페이지 업데이트

--- src/main/java/com/dalrun/service/DotMapService.java
+++ src/main/java/com/dalrun/service/DotMapService.java
@@ -1,29 +1,25 @@
 package com.dalrun.service;
 
-import com.dalrun.domain.DotMap;
-import com.dalrun.domain.User;
-import com.dalrun.dto.DotMapDto;
-//import com.dalrun.repository.DotMapRepository;
-//import com.dalrun.repository.UserRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import com.dalrun.dao.DotMapDao;
 
+import com.dalrun.dto.DotMapDto;
+
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class DotMapService {
-//    private final DotMapRepository dotMapRepositoey;
-//    private final UserRepository userRepository;
-//    
-//    public DotMapService(final DotMapRepository dotMapRepositoey ,final UserRepository userRepository) {
-//    	this.dotMapRepositoey=dotMapRepositoey;
-//    	this.userRepository=userRepository;
-//		// TODO Auto-generated constructor stub
-//	}
+	
+	 @Autowired
+	 DotMapDao dao;
 
+
+    
 //    /* CREATE (도트맵 구매) */
 //    @Transactional
 //    public Long saveDot(DotMapDto.Request dotDto, Long userId) {
@@ -34,11 +30,10 @@ public class DotMapService {
 //    }
 
     /* READ (구매자들 정보) */
-//    @Transactional(readOnly = true)
-//    public List<DotMapDto.Response> getDotMapInfo(){
-//        List<DotMap> dotMapList = dotMapRepositoey.findAll();
-//        List<DotMapDto.Response> dotList = dotMapList.stream().map(DotMapDto.Response::new).collect(Collectors.toList());
-//        return dotList;
-//    }
+   
+    public List<DotMapDto> getDotMapInfo(){
+      List<DotMapDto> dolist=dao.getAllDotMap();
+      return dolist;
+   }
 
 }
