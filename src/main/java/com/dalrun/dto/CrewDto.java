e9683d48351b1234499163a9532c57eed410d9e2
realParkYerin <qwwe9907@kakao.com>
2023-04-25 16:07:47 +0900

    feat : 회원가입 로그인 백엔드, 크루 모집 글쓰기 구현, 크루 dto 추가

--- src/main/java/com/dalrun/dto/CrewDto.java
+++ src/main/java/com/dalrun/dto/CrewDto.java
@@ -2,18 +2,20 @@ package com.dalrun.dto;
 
 import java.io.Serializable;
 
+//크루
 public class CrewDto implements Serializable {
 	
-
-	private String crewsetUp; // 크루 소개
-	private String crewcreateDate; // 크루 생성일
-	private String crewupdate; //크루 수정일
-	private int maxmem; // 크루 인원 수
-	private int crewdel; 
-	private int crewlevel;
-	private String crewimg; // 크루 이미지
-	private String crewname; // 크루 이름
-	private int crewscore; // 크루 점수
+	private int crewSeq;
+	private String memId;
+	private String crewSetUp; // 크루 소개
+	private String crewCreateDate; // 크루 생성일
+	private String crewUpdate; //크루 수정일
+	private int maxMem; // 크루 인원 수
+	private int crewDel; 
+	private int crewLevel;
+	private String crewImg; // 크루 이미지
+	private String crewName; // 크루 이름
+	private int crewScore; // 크루 점수
 	private String myrank; // 나의 크루 랭킹 
 	private String crewcolor; // 크루 색상
 	
@@ -21,28 +23,111 @@ public class CrewDto implements Serializable {
 		// TODO Auto-generated constructor stub
 	}
 
-	public CrewDto(String crewsetUp, String crewcreateDate, String crewupdate, int maxmem, int crewdel, int crewlevel,
-			String crewimg, String crewname, int crewscore, String myrank,String crewcolor) {
+	public CrewDto(int crewSeq, String memId, String crewSetUp, String crewCreateDate, String crewUpdate, int maxMem,
+			int crewDel, int crewLevel, String crewImg, String crewName, int crewScore, String myrank,
+			String crewcolor) {
 		super();
-		this.crewsetUp = crewsetUp;
-		this.crewcreateDate = crewcreateDate;
-		this.crewupdate = crewupdate;
-		this.maxmem = maxmem;
-		this.crewdel = crewdel;
-		this.crewlevel = crewlevel;
-		this.crewimg = crewimg;
-		this.crewname = crewname;
-		this.crewscore = crewscore;
+		this.crewSeq = crewSeq;
+		this.memId = memId;
+		this.crewSetUp = crewSetUp;
+		this.crewCreateDate = crewCreateDate;
+		this.crewUpdate = crewUpdate;
+		this.maxMem = maxMem;
+		this.crewDel = crewDel;
+		this.crewLevel = crewLevel;
+		this.crewImg = crewImg;
+		this.crewName = crewName;
+		this.crewScore = crewScore;
 		this.myrank = myrank;
 		this.crewcolor = crewcolor;
 	}
 
-	public String getCrewcolor() {
-		return crewcolor;
+	public int getCrewSeq() {
+		return crewSeq;
 	}
 
-	public void setCrewcolor(String crewcolor) {
-		this.crewcolor = crewcolor;
+	public void setCrewSeq(int crewSeq) {
+		this.crewSeq = crewSeq;
+	}
+
+	public String getMemId() {
+		return memId;
+	}
+
+	public void setMemId(String memId) {
+		this.memId = memId;
+	}
+
+	public String getCrewSetUp() {
+		return crewSetUp;
+	}
+
+	public void setCrewSetUp(String crewSetUp) {
+		this.crewSetUp = crewSetUp;
+	}
+
+	public String getCrewCreateDate() {
+		return crewCreateDate;
+	}
+
+	public void setCrewCreateDate(String crewCreateDate) {
+		this.crewCreateDate = crewCreateDate;
+	}
+
+	public String getCrewUpdate() {
+		return crewUpdate;
+	}
+
+	public void setCrewUpdate(String crewUpdate) {
+		this.crewUpdate = crewUpdate;
+	}
+
+	public int getMaxMem() {
+		return maxMem;
+	}
+
+	public void setMaxMem(int maxMem) {
+		this.maxMem = maxMem;
+	}
+
+	public int getCrewDel() {
+		return crewDel;
+	}
+
+	public void setCrewDel(int crewDel) {
+		this.crewDel = crewDel;
+	}
+
+	public int getCrewLevel() {
+		return crewLevel;
+	}
+
+	public void setCrewLevel(int crewLevel) {
+		this.crewLevel = crewLevel;
+	}
+
+	public String getCrewImg() {
+		return crewImg;
+	}
+
+	public void setCrewImg(String crewImg) {
+		this.crewImg = crewImg;
+	}
+
+	public String getCrewName() {
+		return crewName;
+	}
+
+	public void setCrewName(String crewName) {
+		this.crewName = crewName;
+	}
+
+	public int getCrewScore() {
+		return crewScore;
+	}
+
+	public void setCrewScore(int crewScore) {
+		this.crewScore = crewScore;
 	}
 
 	public String getMyrank() {
@@ -53,78 +138,20 @@ public class CrewDto implements Serializable {
 		this.myrank = myrank;
 	}
 
-	public String getCrewsetUp() {
-		return crewsetUp;
+	public String getCrewcolor() {
+		return crewcolor;
 	}
 
-	public void setCrewsetUp(String crewsetUp) {
-		this.crewsetUp = crewsetUp;
+	public void setCrewcolor(String crewcolor) {
+		this.crewcolor = crewcolor;
 	}
 
-	public String getCrewcreateDate() {
-		return crewcreateDate;
+	@Override
+	public String toString() {
+		return "CrewDto [crewSeq=" + crewSeq + ", memId=" + memId + ", crewSetUp=" + crewSetUp + ", crewCreateDate="
+				+ crewCreateDate + ", crewUpdate=" + crewUpdate + ", maxMem=" + maxMem + ", crewDel=" + crewDel
+				+ ", crewLevel=" + crewLevel + ", crewImg=" + crewImg + ", crewName=" + crewName + ", crewScore="
+				+ crewScore + ", myrank=" + myrank + ", crewcolor=" + crewcolor + "]";
 	}
-
-	public void setCrewcreateDate(String crewcreateDate) {
-		this.crewcreateDate = crewcreateDate;
-	}
-
-	public String getCrewupdate() {
-		return crewupdate;
-	}
-
-	public void setCrewupdate(String crewupdate) {
-		this.crewupdate = crewupdate;
-	}
-
-	public int getMaxmem() {
-		return maxmem;
-	}
-
-	public void setMaxmem(int maxmem) {
-		this.maxmem = maxmem;
-	}
-
-	public int getCrewdel() {
-		return crewdel;
-	}
-
-	public void setCrewdel(int crewdel) {
-		this.crewdel = crewdel;
-	}
-
-	public int getCrewlevel() {
-		return crewlevel;
-	}
-
-	public void setCrewlevel(int crewlevel) {
-		this.crewlevel = crewlevel;
-	}
-
-	public String getCrewimg() {
-		return crewimg;
-	}
-
-	public void setCrewimg(String crewimg) {
-		this.crewimg = crewimg;
-	}
-
-	public String getCrewname() {
-		return crewname;
-	}
-
-	public void setCrewname(String crewname) {
-		this.crewname = crewname;
-	}
-
-	public int getCrewscore() {
-		return crewscore;
-	}
-
-	public void setCrewscore(int crewscore) {
-		this.crewscore = crewscore;
-	}
-
-	
 	
 }
--- src/main/java/com/dalrun/service/MemberService.java
+++ src/main/java/com/dalrun/service/MemberService.java
@@ -1,5 +1,179 @@
 package com.dalrun.service;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dalrun.dao.MemberDao;
+import com.dalrun.dto.MemberDto;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+@Transactional
 public class MemberService {
+	
+	@Autowired
+	MemberDao dao;
+	
+	//아이디 중복체크
+	public boolean idcheck(String memId) {
+		int n = dao.idcheck(memId);
+		return n>0?true:false;
+	}
+	
+	//회원가입
+	public boolean addmember(MemberDto dto) {
+		int n = dao.addmember(dto);
+		return n>0?true:false;
+	}
+	
+	//로그인
+	public MemberDto login(MemberDto dto) {
+		return dao.login(dto);
+	}
+	
+	//카카오 로그인을 통해 얻은 authorize_code를 사용하여 access_token을 발급받는 메소드
+	//2번
+	public String getKakaoAccessToken (String authorize_code) throws Exception {
+		//변수 선언
+	    String access_Token = "";
+	    String refresh_Token = "";
+	    //reqURL: String 타입의 카카오 로그인 인증서버의 URL
+	    String reqURL = "https://kauth.kakao.com/oauth/kakao/callback";
+	    
+	    try {
+	    	//URL 객체 생성
+			URL url = new URL(reqURL);
+
+			//HttpURLConnection 객체 생성 및 설정
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			
+			// POST 요청을 위해 기본값이 false인 setDoOutput을 true로
+			conn.setRequestMethod("POST");
+			conn.setDoOutput(true);
+			
+			// POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+			StringBuilder sb = new StringBuilder();
+			sb.append("grant_type=authorization_code");
+
+			sb.append("&client_id=33861296b5dfb84484e1df231821dd86"); // REST_API키 본인이 발급받은 key 넣어주기
+			sb.append("&redirect_uri=http://localhost:3000/oauth/kakao/callback"); // REDIRECT_URI 본인이 설정한 주소 넣어주기
+
+			sb.append("&code=" + authorize_code);
+			bw.write(sb.toString());
+			bw.flush();
+
+			// 결과 코드가 200이라면 성공
+			int responseCode = conn.getResponseCode();
+			System.out.println("responseCode : " + responseCode);
+
+			// 요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line = "";
+			String result = "";
+
+			while ((line = br.readLine()) != null) {
+				result += line;
+			}
+			System.out.println("response body : " + result);
+
+			// jackson objectmapper 객체 생성
+			ObjectMapper objectMapper = new ObjectMapper();
+			// JSON String -> Map
+			Map<String, Object> jsonMap = objectMapper.readValue(result, new TypeReference<Map<String, Object>>() {
+			});
+
+			access_Token = jsonMap.get("access_token").toString();
+			refresh_Token = jsonMap.get("refresh_token").toString();
+
+			System.out.println("access_token : " + access_Token);
+			System.out.println("refresh_token : " + refresh_Token);
+
+			br.close();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return access_Token;
+	}
+	
+	//카카오 API를 이용해 사용자 정보를 가져오는 메서드
+	//3번
+	public HashMap<String, Object> getUserInfo(String access_Token) throws Throwable {
+		// 요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
+		HashMap<String, Object> userInfo = new HashMap<String, Object>();
+		String reqURL = "https://kapi.kakao.com/v2/user/me";
+
+		try {
+			//access token을 이용하여 카카오 api서버에 요청을 보내기위해 HttpURLConnection 객체를 생성
+			URL url = new URL(reqURL);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			//GET 메서드를 사용해 사용자 정보를 요청
+			conn.setRequestMethod("GET");
+
+			// 요청에 필요한 Header에 포함될 내용
+			conn.setRequestProperty("Authorization", "Bearer " + access_Token);
+
+			int responseCode = conn.getResponseCode();
+			System.out.println("responseCode : " + responseCode);
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+			String line = "";
+			String result = "";
+
+			while ((line = br.readLine()) != null) {
+				result += line;
+			}
+			System.out.println("response body : " + result);
+			System.out.println("result type" + result.getClass().getName()); // java.lang.String
+
+			try {
+				// jackson objectmapper 객체 생성
+				//응답 결과는 JSON 형태의 문자열
+				//자바 객체로 변환하기 위해 ObjectMapper 객체를 생성
+				ObjectMapper objectMapper = new ObjectMapper();
+				// JSON String -> Map(readValue() 메서드를 이용하여 JSON 문자열을 자바 Map 객체로 변환)
+				Map<String, Object> jsonMap = objectMapper.readValue(result, new TypeReference<Map<String, Object>>() {
+				});
+
+				System.out.println(jsonMap.get("properties"));
+
+				//변환된 Map 객체에서 필요한 정보를 추출하여 HashMap<String, Object> 타입으로 반환
+				//사용자의 닉네임과 이메일 정보를 반환
+				Map<String, Object> properties = (Map<String, Object>) jsonMap.get("properties");
+				Map<String, Object> kakao_account = (Map<String, Object>) jsonMap.get("kakao_account");
+
+				// System.out.println(properties.get("nickname"));
+				// System.out.println(kakao_account.get("email"));
+
+				String nickname = properties.get("nickname").toString();
+				String email = kakao_account.get("email").toString();
+
+				userInfo.put("nickname", nickname);
+				userInfo.put("email", email);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return userInfo;
+	}
 
 }
+
