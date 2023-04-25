05b2c948737de35f9c438b9933104042ad1b3b98
AnSeonJeong <asj081@naver.com>
2023-04-25 11:17:56 +0900

    feat : 관리자 페이지 - Controller, Service, Dao : 각 테이블의 리스트, 글의 총 수 불러오기 (구현)

--- src/main/java/com/dalrun/controller/AdminController.java
+++ src/main/java/com/dalrun/controller/AdminController.java
@@ -9,16 +9,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dalrun.dto.CompetitionDto;
 import com.dalrun.dto.CrewDto;
 import com.dalrun.dto.MemberDto;
+import com.dalrun.dto.OrderDto;
+import com.dalrun.dto.ProductDto;
+import com.dalrun.dto.ProductInquiryDto;
+import com.dalrun.dto.QnaDto;
 import com.dalrun.dto.SearchParam;
+import com.dalrun.dto.ShoeDto;
 import com.dalrun.service.AdminService;
 
 @RestController
 public class AdminController {
 	@Autowired
 	AdminService service;
-	
+	 
 	private void pageNumber(SearchParam params) {
 	    // 글의 시작과 끝
 	    int pn = params.getPageNumber();  // 0 1 2 3 4
@@ -37,6 +43,8 @@ public class AdminController {
 		return map;
 	}
 	
+	// 각 테이블의 리스트, 글의 총 수 불러오기
+	// 회원 관리
 	@GetMapping(value = "admin_memberlist")
 	public Map<String, Object> memberlist(SearchParam params) {
 	    System.out.println("AdminController memberlist " + new Date());
@@ -59,4 +67,72 @@ public class AdminController {
 	    return getList(crewlist, len);
 	}
 	
+	// 게시물 관리
+	/*
+	@GetMapping(value = "admin_productinquirylist")
+	public Map<String, Object> productinquirylist(SearchParam params) {
+	    System.out.println("AdminController productinquirylist " + new Date());
+	    
+	    pageNumber(params);
+	    List<ProductInquiryDto> productinquirylist = service.productinquirylist(params);
+	    int len = service.getAllProductinquiry(params);
+	    
+	    return getList(productinquirylist, len);
+	}
+	
+	@GetMapping(value = "admin_qnalist")
+	public Map<String, Object> qnalist(SearchParam params) {
+	    System.out.println("AdminController qnalist " + new Date());
+	    
+	    pageNumber(params);	    
+	    List<QnaDto> qnalist = service.qnalist(params);
+		int len = service.getAllQna(params);
+	    
+	    return getList(qnalist, len);
+	}
+
+	@GetMapping(value = "admin_shoereviewlist")
+	public Map<String, Object> shoereviewlist(SearchParam params) {
+	    System.out.println("AdminController shoereviewlist " + new Date());
+	    
+	    pageNumber(params);
+	    List<ShoeDto> shoereviewlist = service.shoereviewlist(params);
+	    int len = service.getAllShoereview(params);
+	    
+	    return getList(shoereviewlist, len);
+	}
+	
+	@GetMapping(value = "admin_competitionlist")
+	public Map<String, Object> competitionlist(SearchParam params) {
+	    System.out.println("AdminController competitionlist " + new Date());
+	    
+	    pageNumber(params);	    
+	    List<CompetitionDto> competitionlist = service.competitionlist(params);
+		int len = service.getAllCompetition(params);
+	    
+	    return getList(competitionlist, len);
+	}
+
+	@GetMapping(value = "admin_productlist")
+	public Map<String, Object> productlist(SearchParam params) {
+	    System.out.println("AdminController productlist " + new Date());
+	    
+	    pageNumber(params);
+	    List<ProductDto> productlist = service.productlist(params);
+	    int len = service.getAllProduct(params);
+	    
+	    return getList(productlist, len);
+	}
+	
+	@GetMapping(value = "admin_orderlist")
+	public Map<String, Object> orderlist(SearchParam params) {
+	    System.out.println("AdminController orderlist " + new Date());
+	    
+	    pageNumber(params);
+	    List<OrderDto> orderlist = service.orderlist(params);
+	    int len = service.getAllOrder(params);
+	    
+	    return getList(orderlist, len);
+	}
+	*/
 }
