cbe203660ad5ce586b0930f3c6bb6bcb40817918
XCEVOR <XCEVOR@GMAIL.COM>
2023-04-28 10:24:02 +0900

    improve: OrderDto 에 productId 세부 추가
    이미 추가되었던 private String productId; 에 Constructor, Getter Setter and
    toString 을 생성하였음.

--- src/main/java/com/dalrun/dto/OrderDto.java
+++ src/main/java/com/dalrun/dto/OrderDto.java
@@ -6,16 +6,17 @@ public class OrderDto implements Serializable{
 	
     private int orderSeq;
     private int orderNumber;
-	private String memId;
-	private String orderName;
-	private String orderAddress;
-	private String orderPhone;
-	private String orderRequirment; // 주문 요청 사항
-	private String orderTotalprice; // 총 구매 금액
-	private String orderDate;
-	private int orderQuantity;
-	private String orderState; // 주문 상태
-	private String deliveryState; // 배송상태
+    private String memId;
+    private String orderName;
+    private String orderAddress;
+    private String orderPhone;
+    private String orderRequirment; // 주문 요청 사항
+    private String orderTotalprice; // 총 구매 금액
+    private String orderDate;
+    private int orderQuantity;
+    private String orderState; // 주문 상태
+    private String deliveryState; // 배송상태
+    private String productId;   // 주문한 상품 아이디
 	
 	public OrderDto() {
 		// TODO Auto-generated constructor stub
@@ -23,7 +24,7 @@ public class OrderDto implements Serializable{
 
     public OrderDto(int orderSeq, int orderNumber, String memId, String orderName, String orderAddress,
             String orderPhone, String orderRequirment, String orderTotalprice, String orderDate, int orderQuantity,
-            String orderState, String deliveryState) {
+            String orderState, String deliveryState, String productId) {
         super();
         this.orderSeq = orderSeq;
         this.orderNumber = orderNumber;
@@ -37,6 +38,7 @@ public class OrderDto implements Serializable{
         this.orderQuantity = orderQuantity;
         this.orderState = orderState;
         this.deliveryState = deliveryState;
+        this.productId = productId;
     }
 
     public int getOrderSeq() {
@@ -135,17 +137,23 @@ public class OrderDto implements Serializable{
         this.deliveryState = deliveryState;
     }
 
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
     @Override
     public String toString() {
         return "OrderDto [orderSeq=" + orderSeq + ", orderNumber=" + orderNumber + ", memId=" + memId + ", orderName="
                 + orderName + ", orderAddress=" + orderAddress + ", orderPhone=" + orderPhone + ", orderRequirment="
                 + orderRequirment + ", orderTotalprice=" + orderTotalprice + ", orderDate=" + orderDate
                 + ", orderQuantity=" + orderQuantity + ", orderState=" + orderState + ", deliveryState=" + deliveryState
-                + "]";
+                + ", productId=" + productId + "]";
     }
 
-   
-	
 	
 
 }
