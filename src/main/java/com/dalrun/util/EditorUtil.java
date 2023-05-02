08db33cedc1cae8a4a5e52c86241628bd68d90e0
XCEVOR <XCEVOR@GMAIL.COM>
2023-05-02 13:05:44 +0900

    test: 업로드 파일이름 변경 테스트

--- src/main/java/com/dalrun/util/EditorUtil.java
+++ src/main/java/com/dalrun/util/EditorUtil.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class EditorUtil {
 
-    // 파일명 -> 변경(time)
+    // 파일명 -> 변경
     
     // myfile.txt -> 342456232.txt
     public static String getNewFileName(String filename) {
@@ -22,4 +22,24 @@ public class EditorUtil {
         return newfilename;
     }
     
+    // myfile.PNG -> ProductCode + Seq.PNG
+    public static String getNewProductCodeFileName(String filename, String productcode, int i) {
+        String newfilename = "";
+        String fpost = "";
+        
+        if (filename.indexOf('.') >= 0) {    // 확장자명이 있음
+            fpost = filename.substring(filename.indexOf('.'));  // .PNG
+            if ( (i + 1) < 10 ) {
+                newfilename = productcode + "-0" + (i + 1) + fpost;     // ADID-LIRA + -0 + 2 + .PNG
+            }
+            else {
+                newfilename = productcode + "-" + (i + 1) + fpost;     // ADID-LIRA + - + 12 + .PNG
+            }
+        }
+        else {     // 확장자명이 없음
+            newfilename = new Date().getTime() + ".back";   // 342456232 + .back
+        }
+        
+        return newfilename;
+    }
 }
